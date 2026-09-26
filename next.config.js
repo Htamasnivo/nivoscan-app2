16fa0ab84cab82c1a76e14dea402e85a84991b07
Htamasnivo <horvath.tamas@nivo.hu>
2026-09-26 19:51:22 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,6 +14,19 @@ const nextConfig = {
       "@sparticuz/chromium",
     ],
   },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+
+      config.externals.push({
+        "playwright-core": "commonjs playwright-core",
+        "@sparticuz/chromium": "commonjs @sparticuz/chromium",
+      });
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
