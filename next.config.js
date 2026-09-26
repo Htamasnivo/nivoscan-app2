7416a36a2aec9212cb8b425d2c705d78ad9907e9
Htamasnivo <horvath.tamas@nivo.hu>
2026-09-26 19:47:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,17 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
 
-module.exports = nextConfig
+  experimental: {
+    serverComponentsExternalPackages: [
+      "playwright-core",
+      "@sparticuz/chromium",
+    ],
+  },
+};
+
+module.exports = nextConfig;
